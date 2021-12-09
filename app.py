9d66dd84c04143c809dc6b1e029c0d941b1857e2
Ahyeonn <ahyeon971030@gmail.com>
2021-12-09 15:01:04 +0900

    Users can create user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -14,8 +14,24 @@ users = db.users
 
 @app.route('/')
 def users_index():
-    """Show all users"""
+    """Show all users."""
     return render_template('users_index.html', users = users.find())
+
+@app.route('/users/new')
+def users_new():
+    """Create user."""
+    return render_template('users_new.html')
+
+@app.route('/users', methods= ['POST'])
+def users_submit():
+    """Submit a user info."""
+    user = {
+        'name': request.form.get('name'),
+        'gender': request.form.get('gender'),
+        'state': request.form.get('state')
+    }
+    users.insert_one(user)
+    return redirect(url_for('users_index'))    
 
 if __name__ == '__main__':
     app.run(debug=True)
